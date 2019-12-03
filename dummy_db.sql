e64cbb305286b819fd8efaf64f432e4f3dca108d
pkd007 <pkd007@wp.pl>
2019-12-03 23:57:39 +0100

    new file:   dummy_db.sql - tworzenie bazy dummy dla kontenera 'php'

---
+++ dummy_db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `test` (`user` VARCHAR(45) NOT NULL ,`passwd` VARCHAR(255) NULL ,`UID` VARCHAR(45) NULL);
+
+
+INSERT INTO `test` (`user`, `passwd`, `UID`) VALUES ('joe', '123', '500');
+INSERT INTO `test` (`user`, `passwd`, `UID`) VALUES ('ben', '1234', '501');
+INSERT INTO `test` (`user`, `passwd`, `UID`) VALUES ('ian', '1235', '502');
+INSERT INTO `test` (`user`, `passwd`, `UID`) VALUES ('bob', '12345', '503');
+INSERT INTO `test` (`user`, `passwd`, `UID`) VALUES ('wes', '12345', '504');
